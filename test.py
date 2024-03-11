8cff3643195f0deaa6fa5e281aa90ebc7e84645e
vamshi-vcd <113637912+vamshi-vcd@users.noreply.github.com>
2024-03-11 15:01:58 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as st
+import seaborn as sns 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+
+
+# In[4]:
+
+
+st.header("This is my flirts app")
+st.header("Build by vasmshi")
+
+
+# In[ ]:
+
+
+
+
